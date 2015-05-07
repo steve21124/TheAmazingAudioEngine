steve21124/TheAmazingAudioEngine

commit 51f9fd761369826d0e9c8743440df09474bf525e

Avoid duplicate latency compensation when rendering groups

TheAmazingAudioEngine/AEAudioController.m
@@ -335,7 +335,8 @@ static OSStatus fillComplexBufferInputProc(AudioConverterRef             inAudio
 
 typedef struct __channel_producer_arg_t {
     AEChannelRef channel;
-    AudioTimeStamp inTimeStamp;
+    AudioTimeStamp timeStamp;
+    AudioTimeStamp originalTimeStamp;
     AudioUnitRenderActionFlags *ioActionFlags;
     int nextFilterIndex;
 } channel_producer_arg_t;
@@ -354,7 +355,7 @@ static OSStatus channelAudioProducer(void *userInfo, AudioBufferList *audio, UIn
                 // Run this filter
                 channel_producer_arg_t filterArg = *arg;
                 filterArg.nextFilterIndex = filterIndex+1;
-                return ((AEAudioControllerFilterCallback)callback->callback)((__bridge id)callback->userInfo, (__bridge AEAudioController *)channel->audioController, &channelAudioProducer, (void*)&filterArg, &arg->inTimeStamp, *frames, audio);
+                return ((AEAudioControllerFilterCallback)callback->callback)((__bridge id)callback->userInfo, (__bridge AEAudioController *)channel->audioController, &channelAudioProducer, (void*)&filterArg, &arg->timeStamp, *frames, audio);
             }
             filterIndex++;
         }
@@ -375,7 +376,7 @@ static OSStatus channelAudioProducer(void *userInfo, AudioBufferList *audio, UIn
         AEChannelGroupRef group = (AEChannelGroupRef)channel->ptr;
         
         // Tell mixer/mixer's converter unit to render into audio
-        status = AudioUnitRender(group->converterUnit ? group->converterUnit : group->mixerAudioUnit, arg->ioActionFlags, &arg->inTimeStamp, 0, *frames, audio);
+        status = AudioUnitRender(group->converterUnit ? group->converterUnit : group->mixerAudioUnit, arg->ioActionFlags, &arg->originalTimeStamp, 0, *frames, audio);
         if ( !checkResult(status, "AudioUnitRender") ) return status;
         
         if ( group->level_monitor_data.monitoringEnabled ) {
@@ -383,7 +384,8 @@ static OSStatus channelAudioProducer(void *userInfo, AudioBufferList *audio, UIn
         }
         
         // Advance the sample time, to make sure we continue to render if we're called again with the same arguments
-        arg->inTimeStamp.mSampleTime += *frames;
+        arg->timeStamp.mSampleTime += *frames;
+        arg->originalTimeStamp.mSampleTime += *frames;
     }
     
     return status;
@@ -415,7 +417,8 @@ static OSStatus renderCallback(void *inRefCon, AudioUnitRenderActionFlags *ioAct
     
     channel_producer_arg_t arg = {
         .channel = channel,
-        .inTimeStamp = timestamp,
+        .timeStamp = timestamp,
+        .originalTimeStamp = *inTimeStamp,
         .ioActionFlags = ioActionFlags,
         .nextFilterIndex = 0
     };

commit 41a5b08b54088eac8734e3121c881b2e6351caf8

Fix AEAudioControllerRenderMainOutput that broke with 51f9fd76

TheAmazingAudioEngine/AEAudioController.m
@@ -1310,7 +1310,8 @@ static OSStatus ioUnitRenderNotifyCallback(void *inRefCon, AudioUnitRenderAction
 BOOL AEAudioControllerRenderMainOutput(AEAudioController *audioController, AudioTimeStamp inTimeStamp, UInt32 inNumberFrames, AudioBufferList *ioData) {
     channel_producer_arg_t arg = {
         .channel = audioController->_topChannel,
-        .inTimeStamp = inTimeStamp,
+        .timeStamp = inTimeStamp,
+        .originalTimeStamp = inTimeStamp,
         .ioActionFlags = 0,
         .nextFilterIndex = 0
     };